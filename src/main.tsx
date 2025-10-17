acd1884aef8b8c260c5e53b6e0e5b15eb89ecea0
Harsh Agarwal <harshag.code@gmail.com>
2025-10-18 01:10:01 +0530

    added-analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import "@radix-ui/themes/styles.css"
 import { Theme } from '@radix-ui/themes'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Theme>
       <App />
+      <Analytics />
     </Theme>
   </BrowserRouter>
 )
